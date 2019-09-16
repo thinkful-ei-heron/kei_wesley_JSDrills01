7561752a52af90434a7c6344f95c07a013fe73d7
wjacobs71086 <w.jacobs.632@gmail.com>
2019-09-16 11:49:31 -0700

    Added year of birth

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
 'use strict';
 function createGreeting(name, age){
-  return `Hi my name is ${name} and I am ${age}`;
+  const yearOfBirth = 2019 - age;
+  return `Hi my name is ${name} and I am ${age}. I was born in ${yearOfBirth}.`;
 }
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('wesley',33);
 console.log(greeting1);
 
 //console.log('Hi my name is Wesley and I am 33');
