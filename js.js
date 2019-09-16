ef6c1728c7e3387b59bb804dad2756ef827cb610
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-16 15:07:43 -0400

    add catch error for parameter validity

--- js.js
+++ js.js
@@ -3,18 +3,21 @@ function getYearOfBirth(age){
   if (age < 0 ){
     throw new Error('Age cannot be negative');
   }
-  return 2019-age;
+  return 2019 - age;
 }
 
 function createGreeting(name, age){
+  if (age === undefined || name === undefined){
+    throw new Error('Arguments not valid');
+  }
   const yearOfBirth = getYearOfBirth(age);
   return `Hi my name is ${name} and I am ${age}. I was born in ${yearOfBirth}.`;
 }
 
 
 try {
-  const greeting1 = createGreeting('wesley',33);
+  const greeting1 = createGreeting('wesley', 33);
   console.log(greeting1);
 } catch(error) {
-  console.log(error);
+  console.error(error);
 }
