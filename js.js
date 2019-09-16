b0a0f430b93e48851f251bf3f586e909d030a0ca
wjacobs71086 <w.jacobs.632@gmail.com>
2019-09-16 11:57:38 -0700

    added try catch

--- js.js
+++ js.js
@@ -1,5 +1,8 @@
 'use strict';
 function getYearOfBirth(age){
+  if (age < 0 ){
+    throw new Error('Age cannot be negative');
+  }
   return 2019-age;
 }
 
@@ -7,5 +10,11 @@ function createGreeting(name, age){
   const yearOfBirth = getYearOfBirth(age);
   return `Hi my name is ${name} and I am ${age}. I was born in ${yearOfBirth}.`;
 }
-const greeting1 = createGreeting('wesley',33);
-console.log(greeting1);
+
+
+try {
+  const greeting1 = createGreeting('wesley',33);
+  console.log(greeting1);
+} catch(error) {
+  console.log(error);
+}
